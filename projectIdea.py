418bf2727171745f113f4d5554c31d41fbed0186
aeibekwe <69613728+aeibekwe@users.noreply.github.com>
2020-09-03 18:27:58 -0700

    quick brainstorm on this project

---
+++ projectIdea.py
@@ -0,0 +1,23 @@
+# Goal. Go to Blackboard, and download all the necessary files for class on a weekly basis... maybe on fridays?
+
+# Steps:
+### MIGHT JUST HAVE TO DO THIS BEGINNING STUFF BY HAND FIRST
+#1. open Blackboard (https://blackboard.usc.edu/webapps/login/)
+#2. login.  Maybe skip straight to (https://login.usc.edu/login/login?spEntityID=https://shibboleth.usc.edu/idp/sp&service=&goto=https://login.usc.edu/sso/SSORedirect/metaAlias/USCRealm/idp?ReqID%3D_cd10a0c9f6b7fce24b61ba9f06d02e91%26index%3Dnull%26acsURL%3Dhttps://shibboleth.usc.edu/idp/sp/Shibboleth.sso/SAML2/POST%26spEntityID%3Dhttps://shibboleth.usc.edu/idp/sp%26binding%3Durn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST)
+#3. wait for or get input from user...
+### MAYBE START EXECUTING THE PROGRAM HERE... THIS IS THE BULK OF THE WORK
+#4. once logged in, there are 5 classes(that's how many I'm in) that need to be checked:
+#       1. Taxation (e.g. repeat this with other classes more or less)
+#           first page (announcements):
+#           https://blackboard.usc.edu/webapps/blackboard/execute/announcement?method=search&context=course_entry&course_id=_259927_1&handle=announcements_entry&mode=view
+#           second page (content):
+#           https://blackboard.usc.edu/webapps/blackboard/content/listContent.jsp?course_id=_259927_1&content_id=_6869906_1&mode=reset
+#           download most recent content (maybe save the url each time a successful download happens, then compare that url to make sure that I'm getting one that I didn't get before...:
+#           https://blackboard.usc.edu/bbcswebdav/pid-6987557-dt-content-rid-32038630_2/xid-32038630_2
+#           save that content to the designated directory:
+#           /Users/aeibekwe/Fall/Taxation/Handouts/
+#           repeat something like the above steps for my other classes...
+#       2. Computer Science for Lawyers
+#       3. First Amendment
+#       4. Contract Drafting and Negotiation
+#       5. Criminal Procedure
