59f72492e12f5df2917b3590c0cd05ee0ca68207
aeibekwe <69613728+aeibekwe@users.noreply.github.com>
2020-10-09 11:53:52 -0700

    adding functionality to get_file_names script.py

--- get_file_names/README.md
+++ get_file_names/README.md
@@ -1,2 +1,6 @@
 
-This function takes a directory and recurses through it in search of all files, which it then prints.  Be sure that your input is the Absolute Path of the directory you want to search without the final '/'...
+This function takes a directory and recurses through it in search of all files, which it then prints.  
+
+Be sure that your input is the Absolute Path of the directory you want to search without the final '/'...
+
+Also, be sure that you're in the get_file_names directory so that results will print to the latest_output.txt file.
--- get_file_names/script.py
+++ get_file_names/script.py
@@ -25,10 +25,41 @@ def get_file_names(file):
             full_file_list[i] = get_file_names(full_file_list[i])
 
     flat = flatten(full_file_list)
-    print('Contents of {0}: {1}\n'.format(file, flat))
+    
     return flat
 
 
-path = input('Specify chosen path: ')
+path = input('Specify chosen path [format = \'/directory\']: ')
+result = get_file_names(path)
 
-get_file_names(path)
+print('Contents of {0} (check latest_output.txt for results): {1}\n'.format(path, result))
+
+file_one = open('latest_output.txt', 'w')
+file_one.write('Contents of {0}:\n\n'.format(path))
+file_one.close()
+
+# This is to add a newline character after each list item
+contents = []
+
+for i in result:
+    i += ' \n'
+    contents.append(i)
+
+file_two = open('latest_output.txt', 'a')
+file_two.writelines(contents)
+file_two.close()
+
+# This is to add a list of items separated by only one space, so that it's easier to copy if need be.
+
+contents_cp = []
+
+for i in result:
+    i += ' '
+    contents_cp.append(i)
+
+file_three = open('latest_output.txt', 'a')
+file_three.write(
+    '\n\nHere is the same list only separated by a single space, to make it easier to copy...\n\n'
+)
+file_three.writelines(contents_cp)
+file_three.close()
