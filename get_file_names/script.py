10499701e91093b7990730b422efebff316a3113
aeibekwe <69613728+aeibekwe@users.noreply.github.com>
2020-10-09 14:17:32 -0700

    adding a little loop to make sure that the inputted path is actually a directory.

--- get_file_names/script.py
+++ get_file_names/script.py
@@ -2,7 +2,6 @@ from os import listdir
 from os.path import isfile, isdir, abspath
 
 # get_file_names will return embedded lists if there is a directory within a directory.  this function will flatten them out.
-
 def flatten(alist):
     result = []
     for i in alist:
@@ -14,7 +13,6 @@ def flatten(alist):
     return result
 
 # this function recursively goes through all the contents in a specified directory and checks whether it is a file or directory.  if it encounters a file, it adds it to a list.  if it encounters a directory, it opens that and repeats.  it doesn't stop until all files are retrieved.
-
 def get_file_names(file):
     file_list = listdir(file)
     full_file_list = []
@@ -29,13 +27,16 @@ def get_file_names(file):
             full_file_list[i] = get_file_names(full_file_list[i])
 
     flat = flatten(full_file_list)
-    
     return flat
 
+# this is to add some exception handling on the user input so if you mess up, you don't have to go back and relaunch the script...
 path = input('Specify chosen path [format = \'/directory\']: ')
+while(isdir(path) != True):
+    path = input('\nUh-Oh, that didn\'t work. You might want to double-check your spelling...\n\n\nSpecify chosen path: ')
+
 result = get_file_names(path)
 
-print('Contents of {0} (check latest_output.txt for results): {1}\n'.format(path, result))
+print('\nContents of {0} (\'cat\' or \'open\' latest_output.txt for results):\n\n{1}'.format(path, result))
 
 # This overwrites the contents of latest_output.txt to make space for new content and writes in the header
 file_one = open('latest_output.txt', 'w')
@@ -57,6 +58,6 @@ for i in result:
     i += ' '
     contents_cp.append(i)
 
-file_two.write('\n\nHere is the same list only separated by a single space, to make it easier to copy...\n\n')
+file_two.write('\nHere is the same list only separated by a single space, to make it easier to copy...\n\n')
 file_two.writelines(contents_cp)
 file_two.close()
