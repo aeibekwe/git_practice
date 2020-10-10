5b9556e7924a0edd00a0240cdb080ffbdbf06b9b
aeibekwe <69613728+aeibekwe@users.noreply.github.com>
2020-10-10 09:02:57 -0700

    adding different formats for results and editing the code to cut down length.

--- get_file_names/README.md
+++ get_file_names/README.md
@@ -4,5 +4,5 @@ This function takes a directory and recurses through it in search of all files, 
 
 Be sure that your input is the Absolute Path of the directory you want to search without the final '/'...
 
-Also, be sure that you're in the get_file_names directory so that results will print to the latest_output.txt file.
+Also, be sure that you're in the get_file_names directory so that results will print to the latest_output files.
 
--- get_file_names/script.py
+++ get_file_names/script.py
@@ -38,31 +38,57 @@ result = get_file_names(path)
 
 print('\nContents of {0} (\'cat\' or \'open -e\' latest_output.txt for results):\n\n{1}'.format(path, result))
 
+# Here's a function that writes to a file
+# file_name = str, option = str, the_content = str/var(containing str), writestyle = default(write), if the_content is a list, then use 'writelines', or input anything else other than write...
+def write_to_file(file_name, option, the_content, write_style='write'):
+    a_file = open(file_name, option)
+    if write_style == 'write':
+        a_file.write(the_content)
+    else:
+        a_file.writelines(the_content)
+    a_file.close()
+
 # This overwrites the contents of latest_output.txt to make space for new content and writes in the header
-file_one = open('latest_output.txt', 'w')
-file_one.write('Contents of {0}:\n\n'.format(path))
-file_one.close()
+write_to_file('latest_output.txt', 'w', '\nContents of {0} with absolute path:\n\n'.format(path))
 
-# This is to add a newline character after each list item
-contents = []
-for i in result:
-    i += ' \n'
-    contents.append(i)
+# Here's a function to modify the end of the results
+def append_to_result(the_result, append_this):
+    contents = []
+    for i in the_result:
+        i += append_this
+        contents.append(i)
+    return contents
 
-file_two = open('latest_output.txt', 'a') #Changed the mode from overWrite to Append
-file_two.writelines(contents)
-file_two.write('\n(You can also copy these contents [as space-separated-values] from copy_latest_output.txt)')
-file_two.close()
+contents = append_to_result(result, '\n') # This now adds a newline after each list item
+write_to_file('latest_output.txt', 'a', contents, 'writelines')
+write_to_file('latest_output.txt', 'a', '\nContents of {0} with relative path:\n\n'.format(path))
+
+# This function removes the root/searched directory from each list item, returning relative paths
+def remove_inputted_path_from_result(the_results, the_path):
+    results_stripped = []
+    inputted_path = the_path + '/'
+    start_index = len(inputted_path)
+    for i in the_results:
+        if the_path in i:
+            i_stripped = i[start_index:]
+            results_stripped.append(i_stripped)
+        else:
+            print('A problem was encountered in the remove_inputted_path_from_result function...')
+    return results_stripped
+
+contents_stripped = remove_inputted_path_from_result(contents, path)
+write_to_file('latest_output.txt', 'a', contents_stripped, 'writelines')
+write_to_file('latest_output.txt', 'a', '\nNote: Check the other output files for other formats.')
 
 # This is to add a list of items separated by only one space, so that it's easier to copy if need be.
 # note: you might want to create a second file for the contents to be copied... that way you can just open that and select all.
-contents_cp = []
-for i in result:
-    i += ' '
-    contents_cp.append(i)
-
-file_three = open('copy_latest_output.txt', 'w')
-file_three.writelines(contents_cp)
-file_three.close()
+contents_cp = append_to_result(result, ' ')
+write_to_file('latest_output.ssv', 'w', contents_cp, 'writelines')
 
 # Here is space for additional code to add a csv file, and a list of files minus the inputted directory path
+contents_csv = append_to_result(result, ',')
+write_to_file('latest_output.csv', 'w', contents_csv, 'writelines')
+
+# This is for a tsv (tab-separated values) file
+contents_tsv = append_to_result(result, '\t')
+write_to_file('latest_output.tsv', 'w', contents_tsv, 'writelines')
